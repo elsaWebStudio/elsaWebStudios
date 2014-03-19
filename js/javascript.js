9d1e1334734b26ca218d22ccafe8773a33aab9da
willnuckles <willnuckles@gmail.com>
2014-03-19 15:57:18 -0500

    new site
    goofin off

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(".menuNav").click(function(){
+  
+	$("html").addClass("openNav");
+  
+});
+	
+$(".close-panel, #landing, #welcome").click(function(){
+  
+	&("html").removeClass("openNav");
+  
+});
