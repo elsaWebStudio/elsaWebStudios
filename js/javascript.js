8a7d770501d49fc27047d598bbd1a4090ef8d7cf
willnuckles <willnuckles@gmail.com>
2014-03-19 17:42:46 -0500

    fixed js issue

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,34 @@
+//handles the hidden menu for mobile
 $(".menuNav").click(function(){
-  
-	$("html").addClass("openNav");
-  
+
+	var htmlClass = $("html").attr('class');
+
+	if (htmlClass != "openNav")
+	  	{
+			$("html").addClass("openNav");
+		}
+	else
+		{
+			$("html").removeClass("openNav");
+		}  
 });
-	
-$(".close-panel, #landing, #welcome").click(function(){
-  
-	&("html").removeClass("openNav");
-  
+
+$(".navLink").click(function(){
+	$("html").removeClass("openNav");
+});
+
+//scrolling effect for links pointing to sections on loaded page
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top +10
+				}, 500);
+				return false;
+			}
+		}
+	});
 });
